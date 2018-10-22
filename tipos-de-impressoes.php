cd7b93dfb2a0d05816d124c57ec82e63b4c3369b
LopesBoa <lopesboa@bakongostudio.com>
2018-10-22 18:26:46 -0300

    Conhecendo os tipos de impressao

---
+++ tipos-de-impressoes.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lopes
+ * Date: 10/22/2018
+ * Time: 7:20 PM
+ */
+
+echo 123;
+
+echo "<br>";
+print 123;
+echo "<br>";
+$varArray = [1,2,3];
+print_r($varArray);
+echo "<br>";
+var_dump(123);
+
